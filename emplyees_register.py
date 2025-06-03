52726806308f3b36611fbd6a7c2657b1f28b2e0a
Kauan dos Santos de Souza <kauan.souza@orsegups.com.br>
2025-06-03 10:30:08 -0300

    Apagado a class do TypeOfTheContract e colocado na pessoa, inserido o campo type_contract para inserir no json

--- emplyees_register.py
+++ emplyees_register.py
@@ -1,13 +1,8 @@
 import json
 from time import sleep
-
-class TypeOfTheContract:
-    def __init__(self,pj, clt):
-        self.pessoa_juridica = pj
-        self.clt = clt
     
 class Pessoa:
-    def __init__(self, name, idade, cpf, genero, salario, endereco, matricula):
+    def __init__(self, name, idade, cpf, genero, salario, endereco, matricula, type_contract):
         self.name = name
         self.idade = idade
         self.cpf = cpf
@@ -15,6 +10,7 @@ class Pessoa:
         self.salario = salario
         self.endereco = endereco
         self.matricula = matricula
+        self.type_contract = type_contract
 
     def get_nome(self):
         return self.name
@@ -47,6 +43,7 @@ class Register(Pessoa):
         self.salario = input("salario: ")
         self.endereco = input("endereco: ")
         self.matricula = input("matricula: ")
+        self.type_contract = input("Tipo de contrato: ")
 
     def to_dict(self):
         return {
@@ -56,13 +53,14 @@ class Register(Pessoa):
             "genero": self.genero,
             "salario": self.salario,
             "endereco": self.endereco,
-            "matricula": self.matricula
+            "matricula": self.matricula,
+            "Tipo de contrato": self.type_contract
         }
 
     def confirm_info(self):
-        response = print(f"Esses dados estão corretos ?\n Nome = {self.name};\n Idade = {self.idade};\n CPF = {self.cpf};\n Genero = {self.genero};\n Salario = {self.salario};\n Endereço = {self.endereco};\n Matrícula = {self.matricula};")
+        response = print(f"Esses dados estão corretos ?\n Nome = {self.name};\n Idade = {self.idade};\n CPF = {self.cpf};\n Genero = {self.genero};\n Salario = {self.salario};\n Endereço = {self.endereco};\n Matrícula = {self.matricula}; Tipo = {self.type_contract}")
         sleep(1)
-        confirm = input("As informações estão corretas? [S] ou [N] ").lower()
+        confirm = input("As informações estão corretas? [S] ou [N]\nR: ").lower()
 
         match confirm:
             case 's':
@@ -75,5 +73,5 @@ class Register(Pessoa):
         with open("info.json", "a", encoding="utf8") as arquivo:
             json.dump(dados, arquivo,ensure_ascii=False)
 
-pessoa = Register("João", 25, "123.456.789-00", "Masculino", 3000, "Rua A, 123", "001")
+pessoa = Register("João", 25, "123.456.789-00", "Masculino", 3000, "Rua A, 123", "001", "PJ")
 pessoa.confirm_info()
