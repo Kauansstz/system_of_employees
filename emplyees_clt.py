dfa16c66ad403129db62129907e1d196b7e8749d
Kauan dos Santos de Souza <kauan.souza@orsegups.com.br>
2025-06-03 15:04:42 -0300

    Realizado o calculo do clt

--- emplyees_clt.py
+++ emplyees_clt.py
@@ -1,20 +1,44 @@
-from emplyees_register import Pessoa
-class Clt(Pessoa):
+import json
+
+with open("info.json", "r", encoding="utf-8") as arquivo:
+    base = json.load(arquivo)
+
+class Employees:
+    def __init__(self, name, salario):
+        self.name = name
+        self.contrato = base["Tipo de contrato"]
+        self.salario = salario
+
+    def descontos(self):
+        VT = 8/100
+        VA = 6/100
+
+        desconto_va = self.salario * VA
+        desconto_6 = self.salario - desconto_va
+
+        desconto_vt = desconto_6 * VT
+        salario_final = desconto_6 - desconto_vt
+        return (f"Valor do salário é {salario_final}")
+
+    def mostrar_salario(self):
+        print(f'Salario com descontos: {self.descontos()}')
+
+class Clt(Employees):
     def __init__(self, name, salario):
         super().__init__(name, salario)
+        match name:
+            case name if name in base["nome"]:
+                encontrado = True
+                match self.contrato:
+                    case  self.contrato if  self.contrato == "CLT":
+                        print(f"Colaborador: {name}")
+                        print(f"Tipo de contrato: {base['Tipo de contrato']}")
+                        print(f'Salário Base: {salario}')
+                        self.mostrar_salario()
+                    case _:
+                        ...
+            case _:
+                print("Colaborador não existe")
 
-    def vale_transporte_valor(self):
-        self.vale_transporte = 8
-    
-    
 
-    def vale_alimentacao_valor(self):
-        self.vale_alimentacao = 6
-    
-    def calculo_salario(self):
-        nome = self.nome
-        VA = self.vale_alimentacao
-        VT = self.vale_transporte
-        salario = self.salario
-
-        result = salario - VA - VT
+l = Clt("João", 3000)
